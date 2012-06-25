059390aa01c99ba93a7169f0b951fbfbb0334839
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2012-06-25 12:50:29 -0500

    + funcion global para las vistas para asignar icono por estatus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+def get_icon_name(estatus)
+  case estatus
+  when "Activa"
+    return "activa.png"
+  when "En espera"
+    return "en_espera.png"
+  when "Finalizado"
+    return "finalizado.png"
+  else
+    return "atender.png"
+  end
+end
+
 end
