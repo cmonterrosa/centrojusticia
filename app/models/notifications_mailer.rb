a0f42f8768dc2e7d5bdd14c69dfcfdbff65563b5
Carlos Augusto Monterrosa Lopez <cmonterrosa@gmail.com>
2012-10-03 15:58:46 -0500

    + Metodo para envio de notificacion al actualizar sesion

--- app/models/notifications_mailer.rb
+++ app/models/notifications_mailer.rb
@@ -16,6 +16,7 @@ class NotificationsMailer < ActionMailer::Base
   #---- sesiones ----
   def sesion_created(tipo, sesion)
       user = (tipo=="mediador" ? sesion.mediador : sesion.comediador)
+      tipo = "especialista" if tipo=="mediador"
       setup_email(user)
       @sesion = Sesion.find(sesion)
       @tramite = @sesion.tramite
@@ -28,7 +29,16 @@ class NotificationsMailer < ActionMailer::Base
       setup_email(user)
       @sesion = Sesion.find(sesion)
       @tramite = @sesion.tramite
-      @subject    += "Se ha cancelado sesion programada para el día #{@fecha.strftime("%d de %B de %Y").upcase} / #{@sesion.hora_completa}"
+      @subject    += "Se ha cancelado sesion programada para el día #{@sesion.fecha.strftime("%d de %B de %Y").upcase} / #{@sesion.hora_completa}"
+      @body[:url]  = "http://#{SITE_URL}/"
+  end
+
+  def sesion_updated(tipo, sesion)
+      user = (tipo=="mediador" ? sesion.mediador : sesion.comediador)
+      setup_email(user)
+      @sesion = Sesion.find(sesion)
+      @tramite = @sesion.tramite
+      @subject    += "Se ha reprogramado una sesión para el día #{@sesion.fecha.strftime("%d de %B de %Y").upcase} / #{@sesion.hora_completa}"
       @body[:url]  = "http://#{SITE_URL}/"
   end
 
