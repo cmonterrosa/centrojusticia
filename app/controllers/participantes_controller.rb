141f6f3e0cffe14579c0ec2df6aec0df5ebed3b5
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-11-23 10:32:58 -0600

    + Add a delete method and modify the parameters, now we send tramite id

--- app/controllers/participantes_controller.rb
+++ app/controllers/participantes_controller.rb
@@ -16,10 +16,25 @@ class ParticipantesController < ApplicationController
     @participante.user = current_user
     if @participante.save
       flash[:notice] = "Guardado correctamente"
-      redirect_to :controller => "comparecencias", :action => "new_or_edit", :id => @participante.comparecencia
+      redirect_to :controller => "comparecencias", :action => "new_or_edit", :id => @participante.comparecencia.tramite
     else
       flash[:notice] = "No se pudo guardar, verifique"
       render :action => "new_or_edit"
     end
  end
+
+  def delete
+    if Participante.exists?(params[:id])
+        @participante =  Participante.find(params[:id])
+        @tramite = @participante.comparecencia.tramite
+        if @participante.destroy
+            flash[:notice] = "Participante eliminado correctamente"
+            redirect_to :controller => "comparecencias", :action => "new_or_edit", :id => @tramite
+        else
+            flash[:notice] = "No se pudo eliminar, verifique"
+            render :action => "new_or_edit"
+        end
+    end
+  end
+
 end
