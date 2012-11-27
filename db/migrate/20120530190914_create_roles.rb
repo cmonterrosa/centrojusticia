3551751e8e497cd40f4989068ae76c9371442a0c
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-11-27 11:25:55 -0600

    + New role called lecturaagenda for only read a schedule of sessions

--- db/migrate/20120530190914_create_roles.rb
+++ db/migrate/20120530190914_create_roles.rb
@@ -12,14 +12,15 @@ class CreateRoles < ActiveRecord::Migration
     Role.create(:name => "direccion", :descripcion => "Dirección General", :prioridad => 3)
     Role.create(:name => "subdireccion", :descripcion => "Subdirección Regional", :prioridad => 4)
     Role.create(:name => "controlagenda", :descripcion => "Control de agenda", :prioridad => 5)
-    Role.create(:name => "jefeatencionpublico", :descripcion => "Jefatura de área de Atención al público", :prioridad => 6)
-    Role.create(:name => "atencionpublico", :descripcion => "Area de Atención al público", :prioridad => 7)
-    Role.create(:name => "especialistas", :descripcion => "Especialistas públicos", :prioridad => 8)
-    Role.create(:name => "invitadores", :descripcion => "Invitadores", :prioridad => 9)
-    Role.create(:name => "reportes", :descripcion => "Reportes", :prioridad => 10)
-    Role.create(:name => "convenios", :descripcion => "Área de convenios", :prioridad => 11)
-    Role.create(:name => "solicitantes", :descripcion => "Solicitantes", :prioridad => 12)
-    Role.create(:name => "invitados", :descripcion => "Invitados", :prioridad => 13)
+    Role.create(:name => "lecturaagenda", :descripcion => "Control de agenda", :prioridad => 6)
+    Role.create(:name => "jefeatencionpublico", :descripcion => "Jefatura de área de Atención al público", :prioridad => 7)
+    Role.create(:name => "atencionpublico", :descripcion => "Area de Atención al público", :prioridad => 8)
+    Role.create(:name => "especialistas", :descripcion => "Especialistas públicos", :prioridad => 9)
+    Role.create(:name => "invitadores", :descripcion => "Invitadores", :prioridad => 10)
+    Role.create(:name => "reportes", :descripcion => "Reportes", :prioridad => 11)
+    Role.create(:name => "convenios", :descripcion => "Área de convenios", :prioridad => 12)
+    Role.create(:name => "solicitantes", :descripcion => "Solicitantes", :prioridad => 13)
+    Role.create(:name => "invitados", :descripcion => "Invitados", :prioridad => 14)
      # generate the join table
     create_table "roles_users", :id => false do |t|
       t.integer "role_id", "user_id"
