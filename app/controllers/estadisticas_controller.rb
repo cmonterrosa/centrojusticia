0b35b52b299e50ce5fb72de3b45280a11d3707a2
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-12-10 10:49:33 -0600

    + new graph for age range and gender

--- app/controllers/estadisticas_controller.rb
+++ app/controllers/estadisticas_controller.rb
@@ -22,6 +22,42 @@ class EstadisticasController < ApplicationController
   end
 
 
+  def grafica_atencion_rango_edad
+      #g = Gruff::Pie.new
+      g = Gruff::Bar.new
+      g.title = "Atención por rango de edades"
+      
+      # --- Rango de edades ---
+      # (1-15) (16-30) (31-45) (46-60) (Más de 60)
+
+      @today = Time.now
+      @sum_1_15 = Participante.count(:fecha_nac, :conditions => ["fecha_nac < ? AND fecha_nac >= ?", @today, @today.years_ago(15)])
+      @sum_16_30 = Participante.count(:fecha_nac, :conditions => ["fecha_nac < ? AND fecha_nac >= ?", @today.years_ago(15), @today.years_ago(30)])
+      @sum_31_45 = Participante.count(:fecha_nac, :conditions => ["fecha_nac < ? AND fecha_nac >= ?", @today.years_ago(30), @today.years_ago(45)])
+      @sum_46_60 = Participante.count(:fecha_nac, :conditions => ["fecha_nac < ? AND fecha_nac >= ?", @today.years_ago(45), @today.years_ago(60)])
+      @sum_60_mas = Participante.count(:fecha_nac, :conditions => ["fecha_nac < ?", @today.years_ago(60)])
+      # --- Datos --
+      g.data("de 1 a 15", [ @sum_1_15])
+      g.data("de 16 a 30", [ @sum_16_30])
+      g.data("de 31 a 45", [ @sum_31_45])
+      g.data("de 46 a 60", [ @sum_46_60])
+      g.data("más de 60", [ @sum_60_mas])
+      send_data(g.to_blob,:disposition => 'inline', :type => 'image/png', :filename => "list.png")
+   end
+
+
+    def grafica_atencion_sexo
+      g = Gruff::Pie.new
+      #g = Gruff::Bar.new
+      g.title = "Orientaciones por sexo"
+      @male = Orientacion.count(:sexo, :conditions => ["sexo = 'M'"])
+      @female = Orientacion.count(:sexo, :conditions => ["sexo = 'F'"])
+      g.data("HOMBRES", [@male])
+      g.data("MUJERES", [@female])
+      send_data(g.to_blob,:disposition => 'inline', :type => 'image/png', :filename => "list.png")
+  end
+
+
   
 
 
