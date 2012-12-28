4163d524438e631782ff7cacdf4e498d749b1966
Carlos Monterrosa <cmonterrosa@gmail.com>
2012-12-28 11:03:18 -0600

    + Add parameters for generate invitation report

--- app/controllers/invitaciones_controller.rb
+++ app/controllers/invitaciones_controller.rb
@@ -3,10 +3,28 @@ class InvitacionesController < ApplicationController
   def index
   end
 
+
+
   def generar
     #@sesion = Sesion.find(params[:id])
+    #
+    @sesion = Sesion.find(params[:id])
+    @tramite = @sesion.tramite
+    @orientacion = Orientacion.find_by_tramite_id(@tramite.id)
+    #
     #@participante = Participante.find_by_comparecencia_id(@sesion.tramite.comparecencia.id)
-    send_doc_jdbc("invitacion", "invitacion", params, output_type = 'pdf')
+    param=Hash.new {|k, v| k[v] = {:tipo=>"",:valor=>""}}
+    param["P_NOMBRE"]={:tipo=>"String", :valor=>"Carlos Augusto Monterrosa López"}
+    param["P_ESPECIALISTA"]={:tipo=>"String", :valor=>"Lic. Amauri Palacios Aquino"}
+    param["P_SUBDIRECTOR"]={:tipo=>"String", :valor=>"Rodrigo Domínguez Moscoso"}
+    param["P_ARTICULO"]={:tipo=>"String", :valor=>"la"}
+    param["P_LUGAR"]={:tipo=>"String", :valor=>"Tuxtla Gutiérrez, Chiapas;"}
+    param["P_FECHA"]={:tipo=>"String", :valor=>"21 de Diciembre de 2012"}
+    param["P_SOLICITANTE"]={:tipo=>"String", :valor=>"C. Norma Espinoza Vázquez"}
+    param["P_FECHA_ORIENTACION"]={:tipo=>"String", :valor=>"17 de febrero de 2012"}
+    param["P_FECHA_SESION"]={:tipo=>"String", :valor=>"05 de marzo, a las 3 de la tarde"}
+
+    send_doc_jdbc("invitacion", "invitacion", param, output_type = 'pdf')
 
     #----- parametros anteriores --
 #    param=Hash.new {|k, v| k[v] = {:tipo=>"",:valor=>""}}
