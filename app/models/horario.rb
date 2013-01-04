628e7e02fae4c9b1251032572d2d8e736ae5564e
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-04 11:20:58 -0600

    + Description for a time with spanish notation

--- app/models/horario.rb
+++ app/models/horario.rb
@@ -11,6 +11,21 @@ class Horario < ActiveRecord::Base
     end
   end
 
+  def hora_completa_turno
+    case self.hora
+    when (1..11)
+      return "a las #{self.hora.to_s} de la mañana"
+    when (12)
+      return "a las 12 del día"
+    when (13)
+      return "a la 1 de la tarde"
+    when (14..18)
+      return "a las #{(self.hora.to_i - 12).to_s} de la tarde"
+    when (19..24)
+      return "a las #{(self.hora.to_i - 12).to_s} de la noche"
+    end
+  end
+
   def descripcion_completa
     hora_completa + " (#{self.sala.descripcion.upcase})"
   end
