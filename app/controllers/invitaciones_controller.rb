1fea8da7155dfe0b6ad86a37a7811afaa293a184
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-04 11:18:02 -0600

    + Modify configuration for invoke an invitacion report

--- app/controllers/invitaciones_controller.rb
+++ app/controllers/invitaciones_controller.rb
@@ -4,29 +4,45 @@ class InvitacionesController < ApplicationController
   end
 
 
+  def firmar
+    
+  end
+
 
   def generar
-    #@sesion = Sesion.find(params[:id])
-    #
-    @sesion = Sesion.find(params[:id])
-    @tramite = @sesion.tramite
-    @orientacion = Orientacion.find_by_tramite_id(@tramite.id)
-    #
-    #@participante = Participante.find_by_comparecencia_id(@sesion.tramite.comparecencia.id)
-    param=Hash.new {|k, v| k[v] = {:tipo=>"",:valor=>""}}
-    param["P_NOMBRE"]={:tipo=>"String", :valor=>"Carlos Augusto Monterrosa López"}
-    param["P_ESPECIALISTA"]={:tipo=>"String", :valor=>"Lic. Amauri Palacios Aquino"}
-    param["P_SUBDIRECTOR"]={:tipo=>"String", :valor=>"Rodrigo Domínguez Moscoso"}
-    param["P_ARTICULO"]={:tipo=>"String", :valor=>"la"}
-    param["P_LUGAR"]={:tipo=>"String", :valor=>"Tuxtla Gutiérrez, Chiapas;"}
-    param["P_FECHA"]={:tipo=>"String", :valor=>"21 de Diciembre de 2012"}
-    param["P_SOLICITANTE"]={:tipo=>"String", :valor=>"C. Norma Espinoza Vázquez"}
-    param["P_FECHA_ORIENTACION"]={:tipo=>"String", :valor=>"17 de febrero de 2012"}
-    param["P_FECHA_SESION"]={:tipo=>"String", :valor=>"05 de marzo, a las 3 de la tarde"}
+    @invitacion = Invitacion.find(params[:id])
+    if @invitacion
+       @sesion = @invitacion.sesion
+       @tramite = @sesion.tramite
+       @involucrado = Participante.find(:first, 
+                      :conditions => ["comparecencia_id = ?", Comparecencia.find_by_tramite_id(@tramite).id])
+       #-- Parametros
+       param=Hash.new {|k, v| k[v] = {:tipo=>"",:valor=>""}}
+       #param["P_NOMBRE"]={:tipo=>"String", :valor=>"Carlos Augusto Monterrosa López"}
+       param["P_NOMBRE"]={:tipo=>"String", :valor=>@involucrado.nombre_completo}
+       #param["P_ESPECIALISTA"]={:tipo=>"String", :valor=>"Lic. Amauri Palacios Aquino"}
+       param["P_ESPECIALISTA"]={:tipo=>"String", :valor=>@invitacion.especialista}
+       #param["P_SUBDIRECTOR"]={:tipo=>"String", :valor=>"Rodrigo Domínguez Moscoso"}
+       param["P_SUBDIRECTOR"]={:tipo=>"String", :valor=>@invitacion.subdireccion.titular}
+       #param["P_ARTICULO"]={:tipo=>"String", :valor=>"la"}
+       param["P_ARTICULO"]={:tipo=>"String", :valor=>@invitacion.articulo}
+       #param["P_LUGAR"]={:tipo=>"String", :valor=>"Tuxtla Gutiérrez, Chiapas;"}
+       param["P_LUGAR"]={:tipo=>"String", :valor=>"#{@invitacion.subdireccion.municipio.descripcion}, CHIAPAS;"}
+       #param["P_FECHA"]={:tipo=>"String", :valor=>"21 de Diciembre de 2012"}
+       param["P_FECHA"]={:tipo=>"String", :valor=>"#{Time.now.strftime('%d de %B de %Y')}"}
+       #param["P_SOLICITANTE"]={:tipo=>"String", :valor=>"C. Norma Espinoza Vázquez"}
+       param["P_SOLICITANTE"]={:tipo=>"String", :valor=>@invitacion.orientacion.solicitante}
+       #param["P_FECHA_ORIENTACION"]={:tipo=>"String", :valor=>"17 de febrero de 2012"}
+       param["P_FECHA_ORIENTACION"]={:tipo=>"String", :valor=>"#{@invitacion.orientacion.created_at.strftime('%d de %B de %Y')}"}
+       #param["P_FECHA_SESION"]={:tipo=>"String", :valor=>"05 de marzo, a las 3 de la tarde"}
+       param["P_FECHA_SESION"]={:tipo=>"String", :valor=>"#{@invitacion.fecha_sesion}"}
+       param["P_ARTICULO_ESP"] = {:tipo => "String", :valor => @invitacion.articulo_esp}
+       send_doc_jdbc("invitacion", "invitacion", param, output_type = 'pdf')
+    else
+      render :text => "Imposible generar invitación, verifique los parámetros"
+    end
 
-    send_doc_jdbc("invitacion", "invitacion", param, output_type = 'pdf')
-
-    #----- parametros anteriores --
+#----- parametros anteriores --
 #    param=Hash.new {|k, v| k[v] = {:tipo=>"",:valor=>""}}
 #    param["P_FOLIO"]={:tipo=>"String", :valor=>params[:ciclo]}
 #    param["P_LUGAR"]={:tipo=>"String", :valor=>REPORTS_DIR}
@@ -45,11 +61,10 @@ class InvitacionesController < ApplicationController
   def list_by_tramite
     @tramite = Tramite.find(params[:id])
     @sesiones = Sesion.find(:all, :conditions => ["tramite_id = ?", @tramite.id])
-    @invitaciones= (@sesiones.empty?)? nil :  Invitacion.find(:all, :conditions => ["sesion_id in ?", @sesiones.collect{|s| s.id}])
-    #@invitaciones = Invitacion.find(:all, :conditions => ["sesion_id in ?", @sesiones.collect{|s| s.id}])
+    @invitaciones= (@sesiones.empty?)? nil :  Invitacion.find(:all, :conditions => ["sesion_id in (?)", @sesiones.collect{|s| s.id}])
   end
 
-  def list_by_person
+  def list_by_user
 
   end
 
