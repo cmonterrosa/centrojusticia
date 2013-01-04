99458856fab132d2079358dc46daf72b9ee1c1eb
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-01-04 11:22:24 -0600

    + Upcase full name before save

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -3,8 +3,16 @@ class Participante < ActiveRecord::Base
   belongs_to :municipio
   belongs_to :user
 
+  def before_save
+    self.paterno.upcase! if self.paterno
+    self.materno.upcase! if self.materno
+    self.nombre.upcase! if self.nombre
+  end
+
   def nombre_completo
     "#{self.paterno} #{self.materno} #{self.nombre}"
   end
+
+
 
 end
