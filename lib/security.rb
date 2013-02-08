c0e90b241f39399e0fe4f7c7c00c31003594ae3b
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-02-08 15:46:13 -0600

    + Modification of security tokens generation

--- lib/security.rb
+++ lib/security.rb
@@ -4,11 +4,12 @@ require 'base64'
 module Security
 
   def generate_token
-    Base64::encode64(rand(10).to_s)
+    id = (rand(10)).to_s.rjust(2,'0') + Array.new(4) { (rand(122-97) + 97).chr }.join + (rand(10000)).to_s.rjust(5,'1')
+    Base64::encode64(id.to_s)
   end
   
   def validate_token(token)
-    (Base64::decode64(token.to_s) =~ /^\d{1}$/) ? true : false
+    (Base64::decode64(token.to_s) =~  /^\d{2}[a-z]{4}\d{5}$/) ? true : false
   end
 
 end
