44fd30552124519e5a68a1c504cae8f53c2258fd
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-02-13 11:18:04 -0600

    + Modify of methods for display the time of a sesion in full format

--- app/models/invitacion.rb
+++ app/models/invitacion.rb
@@ -35,13 +35,9 @@ class Invitacion < ActiveRecord::Base
      return "#{@sesion.fecha.strftime('%d de %B')}, #{Horario.find(@sesion.horario_id).hora_completa_turno}"
    end
 
-#    param["P_NOMBRE"]={:tipo=>"String", :valor=>"Carlos Augusto Monterrosa López"}
-#    param["P_ESPECIALISTA"]={:tipo=>"String", :valor=>"Lic. Amauri Palacios Aquino"}
-#    param["P_SUBDIRECTOR"]={:tipo=>"String", :valor=>"Rodrigo Domínguez Moscoso"}
-#    param["P_ARTICULO"]={:tipo=>"String", :valor=>"la"}
-#    param["P_LUGAR"]={:tipo=>"String", :valor=>"Tuxtla Gutiérrez, Chiapas;"}
-#    param["P_FECHA"]={:tipo=>"String", :valor=>"21 de Diciembre de 2012"}
-#    param["P_SOLICITANTE"]={:tipo=>"String", :valor=>"C. Norma Espinoza Vázquez"}
-#    param["P_FECHA_ORIENTACION"]={:tipo=>"String", :valor=>"17 de febrero de 2012"}
-#    param["P_FECHA_SESION"]={:tipo=>"String", :valor=>"05 de marzo, a las 3 de la tarde"}
+   def invitador
+     return User.find(self.invitador_id) if self.invitador_id
+     return nil
+   end
+
 end
