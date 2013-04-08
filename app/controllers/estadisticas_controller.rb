402ce2ac9ba480693cabcfc64cf6a1ac5242432c
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-04-08 11:03:21 -0500

    + Modifications for display a graph with age range

--- app/controllers/estadisticas_controller.rb
+++ app/controllers/estadisticas_controller.rb
@@ -7,23 +7,75 @@ class EstadisticasController < ApplicationController
 
   end
 
+
+    def select_grafica_orientaciones_especialistas
+     @title = "Seleccione el rango de fechas"
+     @action = "grafica_orientaciones_especialistas"
+     return render(:partial => 'select_date_range', :layout => 'only_jquery')
+  end
+
+
+  def grafica_orientaciones_especialistas
+      g = Gruff::Pie.new
+      #g = Gruff::Bar.new
+      g.title = "Orientaciones"
+      @especialistas =  Role.find(:first, :conditions => ["name = ?", 'especialistas']).users
+      if params[:fecha_inicio] && params[:fecha_fin]
+          params[:fecha_fin] = (params[:fecha_inicio]==params[:fecha_fin]) ? params[:fecha_fin] + " 23:59" : params[:fecha_fin]
+          @inicio, @fin = DateTime.parse(params[:fecha_inicio]), DateTime.parse(params[:fecha_fin] + " 23:59")
+          g.title = "Orientaciones del #{@inicio.strftime('%d/%m')} al #{@fin.strftime('%d/%m')}"
+          @especialistas.each do |especialista|
+            total_sesiones = Orientacion.count(:id, :conditions => ["user_id = ? AND fechahora between ? AND ? ", especialista.id.to_i, @inicio, @fin])
+              if total_sesiones > 0
+                g.data("#{especialista.nombre}", [total_sesiones])
+              end
+            end
+      else
+            g.title = "Total de orientaciones"
+            @especialistas.each do |especialista|
+            total_sesiones = Orientacion.count(:id, :conditions => ["user_id = ?", especialista.id.to_i])
+              if total_sesiones > 0
+                g.data("#{especialista.nombre}", [total_sesiones])
+              end
+            end
+      end
+     g.labels = {0=>'Número por especialista'}
+    send_data(g.to_blob,:disposition => 'inline', :type => 'image/png', :filename => "list.png")
+  end
+
+
+
+
   def select_grafica_sesiones_especialistas
-     @title = "Seleccion de especialistas"
-     @action = "grafica_sesiones_especialistas"
+     @title = "Seleccione el rango de fechas"
+     @action = "grafica_orientaciones_especialistas"
      return render(:partial => 'select_date_range', :layout => 'only_jquery')
   end
 
 
   def grafica_sesiones_especialistas
-      g = Gruff::Pie.new
-      #g = Gruff::Bar.new
-      g.title = "Sesiones por especialista"
+      #g = Gruff::Pie.new
+      g = Gruff::Bar.new
+      g.title = "Sesiones por especialista del "
       @especialistas =  Role.find(:first, :conditions => ["name = ?", 'especialistas']).users
-      @especialistas.each do |especialista|
-        total_sesiones = Sesion.count(:id, :conditions => ["mediador_id = ? and activa=true", especialista.id.to_i])
-        if total_sesiones > 0
-          g.data("#{especialista.nombre}", [total_sesiones])
-        end
+      if params[:fecha_inicio] && params[:fecha_fin]
+          params[:fecha_fin] = (params[:fecha_inicio]==params[:fecha_fin]) ? params[:fecha_fin] + " 23:59" : params[:fecha_fin]
+          @inicio, @fin = DateTime.parse(params[:fecha_inicio]), DateTime.parse(params[:fecha_fin] + " 23:59")
+          g.title = "Sesiones por especialista del #{@inicio.strftime('%d')} al #{@fin.strftime('%d de %m')}"
+          @especialistas.each do |especialista|
+            total_sesiones = Sesion.count(:id, :conditions => ["mediador_id = ? AND activa=true AND fecha between ? AND ? ", especialista.id.to_i, @inicio, @fin])
+              if total_sesiones > 0
+                g.data("#{especialista.nombre}", [total_sesiones])
+              end
+            end
+      else
+             g.title = "Sesiones por especialistas"
+            @especialistas.each do |especialista|
+            total_sesiones = Sesion.count(:id, :conditions => ["mediador_id = ? AND activa=true", especialista.id.to_i])
+              if total_sesiones > 0
+                g.data("#{especialista.nombre}", [total_sesiones])
+              end
+            end
       end
     send_data(g.to_blob,:disposition => 'inline', :type => 'image/png', :filename => "list.png")
   end
@@ -61,6 +113,7 @@ class EstadisticasController < ApplicationController
       @female = Orientacion.count(:sexo, :conditions => ["sexo = 'F'"])
       g.data("HOMBRES", [@male])
       g.data("MUJERES", [@female])
+      g.labels = {0=>'Número de orientaciones'}
       send_data(g.to_blob,:disposition => 'inline', :type => 'image/png', :filename => "list.png")
   end
 
