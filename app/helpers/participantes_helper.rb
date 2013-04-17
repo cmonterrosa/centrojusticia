b59584eca0473536a52c787dabf1ef0f99bf40e0
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-04-17 15:48:18 -0500

    + Add a method for return a hash with the years currently

--- app/helpers/participantes_helper.rb
+++ app/helpers/participantes_helper.rb
@@ -1,2 +1,13 @@
 module ParticipantesHelper
+
+
+
+  def hash_of_years
+    hash={}
+    (Time.now.year.to_i-99..Time.now.year).each do |y|
+      hash[(Time.now.year-y).to_s.rjust(2, "0")] = y.to_s
+    end
+    return hash.sort
+  end
+
 end
