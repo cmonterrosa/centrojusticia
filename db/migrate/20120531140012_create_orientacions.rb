b3dc3d067c9e6fc9c0ebaebab0f315eafb3b3229
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-04-23 14:45:55 -0500

    + Make the field direccion (address) bigger

--- db/migrate/20120531140012_create_orientacions.rb
+++ db/migrate/20120531140012_create_orientacions.rb
@@ -9,7 +9,7 @@ class CreateOrientacions < ActiveRecord::Migration
       t.string :materno, :limit => 40
       t.string :nombre, :limit => 60
       t.string :sexo, :limit => 1
-      t.string :direccion, :limit => 60
+      t.string :direccion, :limit => 160
       t.string :telefono, :limit => 10
       t.string :correo, :limit => 40
       t.integer :municipio_id
