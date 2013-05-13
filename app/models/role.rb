d6ee90a53165a8bd9e5f57afe8820706e88fd020
Carlos Monterrosa <cmonterrosa@gmail.com>
2013-05-13 10:24:34 -0500

    + New method users available for specific role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,17 @@
 class Role < ActiveRecord::Base
    has_and_belongs_to_many :users
    has_and_belongs_to_many :estatus
+
+  
+
+  def usuarios_disponibles
+        return User.find_by_sql("SELECT users.* from users inner join roles_users ru on users.id=ru.user_id 
+                                 INNER JOIN roles r on ru.role_id=r.id
+                                 WHERE users.id not in (select user_id as id from movimientos where ('#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}' BETWEEN fecha_inicio AND fecha_fin))
+                                 AND r.name='#{self.name}'")
+
+  end
+
 #  def users
 #      return User.find(:all, :select => "u.*", :joins => "u, roles_users ru", :conditions => ["u.id=ru.user_id AND ru.role_id=?", self.id], :order => "u.login, u.paterno, u.materno")
 #  end
