62659d3036068288c847fbc93955ec53e9300b38
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-06-02 15:49:55 -0500

    + Solo admin puede eliminar tramites

--- app/controllers/tramites_controller.rb
+++ app/controllers/tramites_controller.rb
@@ -3,7 +3,7 @@ class TramitesController < ApplicationController
   before_filter :login_required
   require_role [:cancelatramite, :direccion], :for => [:cancel]
   require_role [:especialistas, :subdireccion, :direccion, :convenios], :for => [:menu]
-  require_role [:admin, :captura], :for => [:destroy]
+  require_role [:admin], :for => [:destroy]
 
 
   def index
