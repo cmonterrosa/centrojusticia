c31ff47ae28d5da8d979bf13a6ccd91e670d27f7
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-06-03 12:33:22 -0500

    + Agrego locales

--- config/environment.rb
+++ config/environment.rb
@@ -39,17 +39,17 @@ Rails::Initializer.run do |config|
 
 
     #--- for save sesions ----
-    config.action_controller.session_store = :active_record_store
+    #config.action_controller.session_store = :active_record_store
 
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  #config.time_zone = 'UTC'
-  #config.time_zone = 'Mexico City'
+  config.time_zone = 'UTC'
+  config.time_zone = 'Mexico City'
 
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :esMX
   #config.gem 'will_paginate', :version => '~> 2.3.16'
 end
