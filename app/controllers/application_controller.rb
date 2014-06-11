f05b59bed86a4a5557aa7f27cd415c5a7ccdd355
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-06-11 11:24:42 -0500

    + Incluyo requires de will paginate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+require 'will_paginate'
+require 'will_paginate/array'
+
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
