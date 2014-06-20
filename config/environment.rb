cce8d4efdad92642b987716b7946f79d2fc0540e
carlos monterrosa <cmonterrosa@cejatapachula.(none)>
2014-06-19 19:51:28 -0500

    + Nuevas configuraciones

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file
-require 'rubygems'
-require 'will_paginate'
-
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
@@ -23,9 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  
-    
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -36,24 +30,14 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-    config.active_record.observers = :UserObserver#, :TramiteObserver, :OrientacionObserver
-
-
-    #--- for save sesions ----
-    #config.action_controller.session_store = :active_record_store
-    #config.action_controller.session_store = :active_record_store
-
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  #config.time_zone = 'UTC'
-  #config.time_zone = 'Mexico City'
-
+  config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  # config.i18n.default_locale = :de
   config.i18n.default_locale = :esMX
-  #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  #config.i18n.default_locale = :esMX
-  #config.gem 'will_paginate', :version => '~> 2.3.16'
+  config.gem 'will_paginate', :version => '~> 2.3.16'
 end
