0ae692d4a2d81c1f3a48d5d4059d21273301dae7
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-06-30 12:45:37 -0500

    + Agregue funcion para saber si sala esta libre en horario y fecha especifica

--- app/models/sala.rb
+++ app/models/sala.rb
@@ -1,6 +1,17 @@
+require 'date'
 class Sala < ActiveRecord::Base
   belongs_to :subdireccion
   has_many :orientacions
   has_many :horarios
   has_many :sesions
+
+  def libre?(horario_id = nil, fecha=nil)
+    @ocupada=nil
+    if horario_id && fecha
+      fecha = fecha.strftime("%Y-%m-%d")
+      @ocupada = Sesion.find(:first, :conditions => ["fecha= ? AND sala_id = ? AND horario_id = ? AND activa = ?", fecha, self.id, horario_id, true])
+    end
+    return ((@ocupada)? false  : true)
+  end
+
 end
