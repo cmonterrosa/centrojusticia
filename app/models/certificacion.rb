01bbe37e720b63698f66503038909af6a6a6c01b
Carlos Monterrosa <cmonterrosa@gmail.com>
2014-11-14 10:34:19 -0600

    + Metodo para generar folio autonumerico de certificacion

--- app/models/certificacion.rb
+++ app/models/certificacion.rb
@@ -1,3 +1,13 @@
 class Certificacion < ActiveRecord::Base
   belongs_to :empleado
+
+   def generar_folio
+     unless self.folio
+       maximo=  Certificacion.maximum(:folio)
+        folio = 1 unless maximo
+        folio ||= maximo.to_i + 1
+        self.folio = folio
+     end
+   end
+
 end
