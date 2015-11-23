0e1ce497af1d3a16934ef1d35604acf192faa87f
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-11-23 11:26:45 -0600

    + Nuevo ajuste del procedimiento almacenado

--- db/migrate/20151123163456_ajuste_procedimiento.rb
+++ db/migrate/20151123163456_ajuste_procedimiento.rb
@@ -9,9 +9,11 @@ class AjusteProcedimiento < ActiveRecord::Migration
 BEGIN
 
  DECLARE consecutivo INT;
+ DECLARE anterior_anterior INT;
  DECLARE anterior INT;
  DECLARE contador INT;
  DECLARE contador_anterior INT;
+ DECLARE contador_anterior_anterior INT;
 
  DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
@@ -25,22 +27,29 @@ BEGIN
 
  SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
  START TRANSACTION;
+    SET anterior_anterior=(SELECT Max(folio_expediente) - 2 FROM tramites WHERE anio = v_anio FOR UPDATE);
     SET anterior=(SELECT Max(folio_expediente) - 1 FROM tramites WHERE anio = v_anio FOR UPDATE);
     SET consecutivo=(SELECT Max(folio_expediente) + 1 FROM tramites WHERE anio = v_anio FOR UPDATE);
+    SET contador_anterior_anterior = (SELECT Count(folio_expediente) FROM tramites WHERE anio = v_anio AND folio_expediente = anterior_anterior);
     SET contador_anterior = (SELECT Count(folio_expediente) FROM tramites WHERE anio = v_anio AND folio_expediente = anterior);
     SET contador=(SELECT Count(folio_expediente) FROM tramites WHERE anio = v_anio AND folio_expediente = consecutivo);
-
-    IF (contador_anterior < 1) THEN
-        UPDATE tramites SET folio_expediente=anterior WHERE id=v_id;
+    IF (contador_anterior_anterior < 1) THEN
+        UPDATE tramites SET folio_expediente=anterior_anterior WHERE id=v_id;
     ELSE
-        IF (contador < 1) THEN
-            UPDATE tramites SET folio_expediente=consecutivo WHERE id=v_id;
+        IF (contador_anterior < 1) THEN
+          UPDATE tramites SET folio_expediente=anterior WHERE id=v_id;
+        ELSE
+            IF (contador < 1) THEN
+              UPDATE tramites SET folio_expediente=consecutivo WHERE id=v_id;
+            END IF;
         END IF;
     END IF;
     COMMIT;
 END")
 
-  end
+    execute("DROP PROCEDURE IF EXISTS tramite_anio_folio_expediente;")
+    add_index :tramites, [:anio, :folio_expediente], :name => "tramite_anio_folio_expediente"
+end
 
   def self.down
      execute("DROP PROCEDURE IF EXISTS update_folio_consecutivo;")
